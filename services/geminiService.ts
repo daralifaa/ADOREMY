b5931c340c5b660e21a73788bfe262b0da04e8ea
daralifaa <daralifaa@gmail.com>
2025-12-16 17:22:22 +0700

    Update geminiService.ts

--- services/geminiService.ts
+++ services/geminiService.ts
@@ -1,7 +1,7 @@
 import { GoogleGenAI } from "@google/genai";
 import { ProductType } from "../types";
 
-const ai = new GoogleGenAI({ apiKey: import.meta.env.VITE_API_KEY });
+const ai = new GoogleGenAI({ apiKey: process.env.API_KEY });
 
 export const getGeminiStyleAdvice = async (
   username: string,
